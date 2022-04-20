e8e92bddba46ab5623a7e1362e0372203c5e6665
SAMSUNG PC <smj5704@naver.com>
2022-04-20 15:46:08 +0900

    study

---
+++ src/VariableInitializationExample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int value = sc.nextInt();
+
+        int result = value + 10;
+        System.out.println(result);
+
+
+
+    }
+}
