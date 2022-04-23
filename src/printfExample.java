d5d243c3fe2d8e2e0c0198c4caeda9f91097aee3
SAMSUNG PC <smj5704@naver.com>
2022-04-24 02:00:03 +0900

    study 20220423

--- src/printfExample.java
+++ src/printfExample.java
@@ -3,6 +3,13 @@ public class printfExample {
 public static void main(String[] args) {
     int value = 123;
 //    %d : 정수
-    System.out.printf("상품의 가격 : %d원\n",value);
+//    %6d : 6자리 정수, 왼쪽 빈 자리 공백 ex. 123 -> ___123
+//    %-6d : 6자리 정수, 오른쪽 빈 자리 공백 ex. 123 -> 123___
+//    %06d : 6자리 정수. 왼쪽 빈자리 0 채움 ex. 123 -> 000123
+
+    System.out.printf("price : %d\n",value);
+    System.out.printf("price : %6d\n",value);
+    System.out.printf("price : %-6d\n",value);
+    System.out.printf("price : %06d\n",value);
 }
 }
