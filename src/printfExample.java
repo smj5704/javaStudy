2ddca311d10e2f318e515583b1288a1f141d797e
SAMSUNG PC <smj5704@naver.com>
2022-04-23 23:46:14 +0900

    study 20220420

---
+++ src/printfExample.java
@@ -0,0 +1,8 @@
+public class printfExample {
+//    printf()메소드 사용법
+public static void main(String[] args) {
+    int value = 123;
+//    %d : 정수
+    System.out.printf("상품의 가격 : %d원\n",value);
+}
+}
