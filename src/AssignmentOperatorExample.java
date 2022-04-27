5650c76b97a559e29b86ebe01e5146693810e85a
SAMSUNG PC <smj5704@naver.com>
2022-04-28 00:40:18 +0900

    study 20220428 혼공자 대입연산자 예제 (124p)

---
+++ src/AssignmentOperatorExample.java
@@ -0,0 +1,20 @@
+public class AssignmentOperatorExample {
+    public static void main(String[] args) {
+        int result = 0;
+//        0 + 10
+        result += 10;
+        System.out.println("result = " + result);
+//        10 - 5
+        result -= 5;
+        System.out.println("result = " + result);
+//        5 x 3
+        result *= 3;
+        System.out.println("result = " + result);
+//        15 / 5(몫)
+        result /= 5;
+        System.out.println("result = " + result);
+//        3 % 3(나머지)
+        result %= 3;
+        System.out.println("result = " + result);
+    }
+}
