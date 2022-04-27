d5e968c2960c2ad5172afbe08483db09d64715ef
SAMSUNG PC <smj5704@naver.com>
2022-04-27 23:23:59 +0900

    study 20220427 혼공자 증감연산자 예제 (112p)

---
+++ src/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,38 @@
+public class IncreaseDecreaseOperatorExample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+        int z;
+
+        System.out.println("----------------");
+        //11
+        x++;
+        //12
+        ++x;
+        System.out.println("x ? " + x);
+
+        System.out.println("----------------");
+        //9
+        y--;
+        //8
+        --y;
+        System.out.println("y ? " + y);
+
+        System.out.println("----------------");
+        //x를 증가시키지 않고 에 먼저 위에 x값을 대입
+        z = x++;
+        //12
+        System.out.println("z ? " + z);
+//        이후에 1증가
+//        13
+        System.out.println("x ? " + x);
+
+        System.out.println("----------------");
+//        x를 증가 시킨 후 z에 대입
+        z = ++x;
+//        14
+        System.out.println("z ? " + z);
+//        14
+        System.out.println("x ? " + x);
+    }
+}
