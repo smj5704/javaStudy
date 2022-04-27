d9c006905c9bce5c701f5a666e8e70974568d849
SAMSUNG PC <smj5704@naver.com>
2022-04-28 00:43:31 +0900

    study 20220428 혼공자 삼항연산자 예제 (124p)

---
+++ src/ConditionalOperationExample.java
@@ -0,0 +1,8 @@
+public class ConditionalOperationExample {
+    public static void main(String[] args) {
+        int score = 85;
+//        90이면 A , 아니면 넘어감-> 80이면 B 아니면 C
+        char grade = (score > 90) ? 'A' : ((score > 80 ) ? 'B' : 'C');
+        System.out.println("score ? " + score + "  grade ? " + grade );
+    }
+}
