502dd333ec8ae7b382ec361b43870639b4e41704
SAMSUNG PC <smj5704@naver.com>
2022-04-28 00:47:12 +0900

    study 20220428 혼공자 chapter 03 연산자 확인문제 4번 삼항연산자 예제 (128p)

---
+++ src/Chap3Quiz4.java
@@ -0,0 +1,14 @@
+public class Chap3Quiz4 {
+//    534자루 연ㅇ필을 30명의 학생들에게 똑같은 개수로 나누어 줄 때 1인당 몇개를 가질 수 있고 몇 개가 남는가
+    public static void main(String[] args) {
+        int pencils = 543;
+        int students = 30;
+//        몫
+        int pencilsPerStudents = pencils / students;
+        System.out.println(pencilsPerStudents);
+//        나머지
+        int pencilsLeft = pencils % students;
+        System.out.println(pencilsLeft);
+    }
+}
+
