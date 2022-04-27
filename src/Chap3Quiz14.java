729ce2c7d3ef89e52e070140c3a137bad1f2c82c
SAMSUNG PC <smj5704@naver.com>
2022-04-28 00:54:23 +0900

    study 20220428 혼공자 chapter 03 연산자 확인문제 14번 (131p)

---
+++ src/Chap3Quiz14.java
@@ -0,0 +1,7 @@
+public class Chap3Quiz14 {
+    public static void main(String[] args) {
+        int score = 85;
+        String result = (!(score > 90)) ? "A" : "B";
+        System.out.println(result);
+    }
+}
