fd752cbb57d40ad7216ea20d063282cd65eb1f92
SAMSUNG PC <smj5704@naver.com>
2022-04-28 00:51:51 +0900

    study 20220428 혼공자 chapter 03 연산자 확인문제 11번 삼항연산자 예제 (130p)

---
+++ src/Chap3Quiz11.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Chap3Quiz11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("id ? ");
+        String id = sc.nextLine();
+
+        System.out.println("pw ? ");
+        String strpw = sc.nextLine();
+        int pw = Integer.parseInt(strpw);
+
+        if(id.equals("java")) {
+            if(pw == 12345) {
+                System.out.println("success!");
+            }else {
+                System.out.println("check your pw");
+            }
+        }else {
+            System.out.println("check your id");
+        }
+
+    }
+}
