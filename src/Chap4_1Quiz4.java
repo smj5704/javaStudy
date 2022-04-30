fdb02afdcf5008ed25ab4b516c867da3b1a3df83
SAMSUNG PC <smj5704@naver.com>
2022-05-01 01:00:41 +0900

    study 20220501 혼공자 chapter 04 조건문과 반복문 확인문제 4번 (147p)

---
+++ src/Chap4_1Quiz4.java
@@ -0,0 +1,20 @@
+public class Chap4_1Quiz4 {
+    public static void main(String[] args) {
+        System.out.println("choice!");
+        char grade = 'C';
+        switch (grade) {
+            case 'A' :
+                System.out.println("VVIP");
+            case 'B' :
+                System.out.println("VIP");
+                break;
+            case 'C' :
+                System.out.println("A");
+            default :
+                System.out.println("B");
+                break;
+        }
+        System.out.println("thank you!");
+
+    }
+}
