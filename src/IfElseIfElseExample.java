c076cfb52f13ba385404cc44cf1750ab93c75df0
SAMSUNG PC <smj5704@naver.com>
2022-05-01 00:25:00 +0900

    study 20220501 혼공자 chapter 04 조건문과 반복문 if-else if문 예제 (139p)

---
+++ src/IfElseIfElseExample.java
@@ -0,0 +1,19 @@
+public class IfElseIfElseExample {
+    public static void main(String[] args) {
+        int score = 75;
+
+        if(score>=90) {
+            System.out.println("score : 90 ~ 100");
+            System.out.println("grade : A");
+        }else if(score>=80){
+            System.out.println("score : 80 ~ 89");
+            System.out.println("grade : B");
+        }else if(score>=70) {
+            System.out.println("score : 70 ~ 79");
+            System.out.println("grade : C");
+        }else {
+            System.out.println("score : 60 down");
+            System.out.println("grade : D");
+        }
+    }
+}
