1a3d29c6fb716989052a1a6888767f751a9d282f
SAMSUNG PC <smj5704@naver.com>
2022-04-30 23:57:18 +0900

    study 20220428 혼공자 chapter 04 조건문과 반복문 if문 예제 (136p)

---
+++ src/IfExample.java
@@ -0,0 +1,15 @@
+public class IfExample {
+    public static void main(String[] args) {
+        int score = 93;
+
+        if(score>=90) {
+            System.out.println("up 90");
+            System.out.println("grade : A ");
+        }
+
+        if(score<90) {
+            System.out.println("down 90");
+            System.out.println("grade : B");
+        }
+    }
+}
