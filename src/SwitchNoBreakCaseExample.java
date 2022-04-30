fcd61354a964e848c1b0ad20334090e51b3fd8c3
SAMSUNG PC <smj5704@naver.com>
2022-05-01 00:41:31 +0900

    study 20220501 혼공자 chapter 04 조건문과 반복문 switch문 예제 - break문이 없는 case (143p)

--- src/SwitchNoBreakCaseExample.java
+++ src/SwitchNoBreakCaseExample.java
@@ -2,12 +2,11 @@ public class SwitchNoBreakCaseExample {
     public static void main(String[] args) {
 //        8~11사이에 정수 뽑기
         int time = (int) (Math.random() * 4) + 8;
-        System.out.println("now ?" + time + "o'clock");
-
+        System.out.println("now ?" + time + " o'clock");
+//       break가 없으면 바로 다음으로 넘어가서 계속 출력된다
         switch (time) {
             case 8:
                 System.out.println("go company");
-                break;
             case 9 :
                 System.out.println("discuss time");
             case 10 :
