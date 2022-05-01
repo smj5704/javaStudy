15022cf1099e3b8319f9cec5927444a411fdb3a3
SAMSUNG PC <smj5704@naver.com>
2022-05-01 23:53:33 +0900

    study 20220501 혼공자 chapter 04 조건문과 반복문 for 문 1부터 100까지 합 출력 (151p)

---
+++ src/ForSumFrom1To100Example.java
@@ -0,0 +1,9 @@
+public class ForSumFrom1To100Example {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i<=100; i++) {
+            sum += i;
+        }
+        System.out.println("From1To100 Sum ? " + sum);
+    }
+}
