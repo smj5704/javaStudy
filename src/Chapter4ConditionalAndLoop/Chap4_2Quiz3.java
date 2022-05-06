92a529542cfabace28fd536579fb13f081053476
SAMSUNG PC <smj5704@naver.com>
2022-05-06 22:49:07 +0900

    study 20220506 혼공자 chapter 04 조건문과 반복문 quiz3번 주사위 두개 합 (158p)

---
+++ src/Chapter4ConditionalAndLoop/Chap4_2Quiz3.java
@@ -0,0 +1,14 @@
+package Chapter4ConditionalAndLoop;
+
+public class Chap4_2Quiz3 {
+    public static void main(String[] args) {
+        while(true) {
+            int num1 = (int) (Math.random() * 6) + 1;
+            int num2 = (int) (Math.random() * 6) + 1;
+            System.out.println("(" + num1 + ", " + num2 + ")");
+            if((num1 + num2) == 5){
+                break;
+            }
+        }
+    }
+}
