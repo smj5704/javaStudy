56c5a09e39b372a6dda8f4175c2be6abeacb2f95
SAMSUNG PC <smj5704@naver.com>
2022-05-07 22:42:28 +0900

    study 20220507 혼공자 chapter 05 참조 타입 문자열 비교 (173p)

---
+++ src/Chapter5ReferenceType/StringEqualsExample.java
@@ -0,0 +1,31 @@
+package Chapter5ReferenceType;
+
+public class StringEqualsExample {
+    public static void main(String[] args) {
+        String strVar1 = "minchul";
+        String strVar2 = "minchul";
+
+        if(strVar1 == strVar2) {
+            System.out.println("Reference is same");
+        }else{
+            System.out.println("Reference is different");
+        }
+
+        if(strVar1.equals(strVar2)){
+            System.out.println("Same String");
+        }
+
+        String strVar3 = new String("minchul");
+        String strVar4 = new String("minchul");
+
+        if(strVar3 == strVar4){
+            System.out.println("Reference is same");
+        }else{
+            System.out.println("Reference is different");
+        }
+
+        if(strVar3.equals(strVar4)){
+            System.out.println("Same String");
+        }
+    }
+}
