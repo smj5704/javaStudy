ccf8e15ad120a9d516660ea3441792cd48ca9d2a
SAMSUNG PC <smj5704@naver.com>
2022-05-10 01:39:12 +0900

    study 20220510 혼공자 chapter 02 변수와 타입

---
+++ src/Chapter2Variable/BooleanExample.java
@@ -0,0 +1,12 @@
+package Chapter2Variable;
+
+public class BooleanExample {
+    public static void main(String[] args) {
+        boolean stop = true;
+        if(stop){
+            System.out.println("stop");
+        }else {
+            System.out.println("start");
+        }
+    }
+}
---
+++ src/Chapter2Variable/StringExample.java
@@ -0,0 +1,10 @@
+package Chapter2Variable;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String name = "hong";
+        String job = "programer";
+        System.out.println(name);
+        System.out.println(job);
+    }
+}
