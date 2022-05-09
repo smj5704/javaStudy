6ab707e47786e453b7bbf0e9552fb79565487eef
SAMSUNG PC <smj5704@naver.com>
2022-05-10 01:33:35 +0900

    study 20220510 혼공자 chapter 02 변수와 타입

---
+++ src/Chapter2Variable/IntegerLiteralExample.java
@@ -0,0 +1,20 @@
+package Chapter2Variable;
+
+public class IntegerLiteralExample {
+    public static void main(String[] args) {
+        //2진수 -> 3
+        int var1 = 0b011;
+        //8진수 -> 134
+        int var2 = 0206;
+        //10진수
+        int var3 = 365;
+        //16진수 -> 179
+        int var4 = 0xB3;
+
+        System.out.println("var1 : " + var1);
+        System.out.println("var2 : " + var2);
+        System.out.println("var3 : " + var3);
+        System.out.println("var4 : " + var4);
+
+    }
+}
