184a653f62e0412614da45f48edc8938372881fc
SAMSUNG PC <smj5704@naver.com>
2022-05-10 01:26:45 +0900

    study 20220510 혼공자 chapter 02 변수와 타입

---
+++ src/Chapter2Variable/CharExample.java
@@ -0,0 +1,25 @@
+package Chapter2Variable;
+
+public class CharExample {
+    public static void main(String[] args) {
+        //문자 직접 저장
+        char c1 = 'A';
+        //10진수 저장
+        char c2 = 65;
+        //16진수 저장
+        char c3 = '\u0041';
+        //문자 직접 저장
+        char c4 = '가';
+        //10진수 저장
+        char c5 = 44032;
+        //16진수 저장
+        char c6 = '\uac00';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println(c5);
+        System.out.println(c6);
+    }
+}
---
+++ src/Chapter2Variable/EscapeExample.java
@@ -0,0 +1,10 @@
+package Chapter2Variable;
+
+public class EscapeExample {
+    public static void main(String[] args) {
+        System.out.println("number\tname\tjob");
+        System.out.println("print\n");
+        System.out.println("we are \"student\"");
+        System.out.println("spring\\summer\\atum\\winter");;
+    }
+}
