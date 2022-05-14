b455cf94a2b014a3294e80bc277b2fbb92c6d007
SAMSUNG PC <smj5704@naver.com>
2022-05-15 05:55:35 +0900

    study 20220515 혼공자 chapter 05 배열 확인문제 4번 (201p)

---
+++ src/Chapter5ReferenceType/Chap5_2Quiz4.java
@@ -0,0 +1,14 @@
+package Chapter5ReferenceType;
+
+public class Chap5_2Quiz4 {
+    public static void main(String[] args) {
+        int max = 0;
+        int[] array = {1,5,3,8,2};
+        for(int i = 0; i < array.length; i++){
+            if(array[i] > max){
+                max = array[i];
+            }
+        }
+        System.out.println("max : " + max);
+    }
+}
